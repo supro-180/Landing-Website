bfc9a9e1267ef5b4efc07a7b56d79f846d451460
supro-180 <71887564+supro-180@users.noreply.github.com>
2020-09-26 00:00:21 +0600

    Added html, css & js files

---
+++ script.js
@@ -0,0 +1,20 @@
+//  var myFunction = document.querySelector(".pepsi").addEventListener(onmouseover , addStyle());
+//  function addStyle() {
+//      var x =  document.querySelector(".sec");
+//      x.style.background = "red";
+// }
+  
+// .addEventListener(click , addStyle1());
+// function addStyle1()
+// {
+//     var x = document.querySelector(".sec")
+//     x.style.background = "red";
+// }
+function imgSlider(anything){
+    document.querySelector(".pepsi").src = anything;
+}
+
+function changeBgColor(color){
+    document.querySelector(".sec").style.background = color;
+    
+}
